37fb5792c0233a5b7191cc92ec348ff3eb1e472c
Alec Chan <alec@alecchan.org>
2017-04-15 08:54:21 +0000

    finished all fixes

--- Servo_Example.py
+++ Servo_Example.py
@@ -3,41 +3,60 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 import xbox
-
+import sys
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40, debug=True)
 joy = xbox.Joystick()
 
 
-servoClockwise = 0.0020
-servoCounterCW = 0.0010
-servoStop      = 0.0015
-
+servoClockwise = 1.6
+servoCounterCW = 1.4
+servoStop      = 1.5
+freq           = 60
 
 def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
+  pulseLength = 1000000
+  pulseLength //= freq
+  pulseLength //=4096
+  pulse *=1000
+  print "pulseLength is %d" % pulseLength
+  pulse //= pulseLength
+  pulse = int(pulse)
+  print "Pulse was %d bits" % pulse
   pwm.setPWM(channel, 0, pulse)
 
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+pwm.setPWMFreq(freq)                        # Set frequency to 60 Hz
 
 
 while (True):
   # Change speed of continuous servo on channel O
   if joy.A():
-    setServoPulse(0,servoClockwise)
+    try:
+      setServoPulse(0,servoClockwise)
+    except:
+      print("Unhandled error:", sys.exc_info()[0])
+      joy.close()
+      raise
   elif joy.B():
-    serServoPulse(0,servoCounterCW)
+    try:
+      setServoPulse(0,servoCounterCW)
+    except:
+      print("Unhandled error:", sys.exc_info()[0])
+      joy.close()
+      raise
   elif joy.X():
-    setServoPulse(0,servoStop)
-    break    
+    try:
+      setServoPulse(0,servoStop)
+      joy.close()
+    except:
+      print("Unhandled error:", sys.exc_info()[0])
+      joy.close()
+      raise
+    break
+  else:
+    setServoPulse(0,servoStop)    
   
 
 
